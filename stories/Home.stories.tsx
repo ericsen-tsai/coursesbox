25d144978d596ae2bab53d3aa8ef7ea74f48a9a1
Ericsen Tsai <bommer95175@gmail.com>
2022-09-03 22:32:53 +0800

    feat: add button component

--- stories/Home.stories.tsx
+++
@@ -1,9 +0,0 @@
-import Home from '../pages/index'
-import { ComponentMeta } from '@storybook/react';
-
-export default {
-  title: 'page/Pages',
-  component: Home
-} as ComponentMeta<typeof Home>;
-
-export const HomePage = () => <Home />
